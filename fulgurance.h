15c5cad0926bcec21543ba213d6322c708a5b63f
Julien <pgyygp090@gmail.com>
2025-01-03 22:07:32 +0100

    df

--- fulgurance.h
+++ fulgurance.h
@@ -1287,20 +1287,22 @@ std::vector<unsigned int> qbinom(std::vector<double> &pvec, unsigned int &n, dou
 //@E unsigned int size = 100;
 //@E double p = 0.5;
 //@E unsigned int n = 60;
-//@E std::vector<unsigned int> out = rbinom(n, size, p);
+//@E std::vector&lt;unsigned int&gt; out = rbinom(n, size, p);
 //@E print_nvec(out);
  //@E :0: 58 42 42 42 59 59 41 41 61 61 39 39 67 67 
  //@E 35 35 50 50 50 50 50 50 50 50
 //@E :25: 50 50 50 50 50 50 50 50 50 50 50 50 50 50 
 //@E 50 50 50 50 50 50 50 50 50 50
 //@E :50: 50 50 50 50 50 50 50 50 50 50
-//@E std::cout << Mean(out) << "\n";
+//@E Mean(out);
 //@E 49
-//@E std::cout << Sd(out) << "\n";
+//@E Sd(out);
 //@E 5.90141
-//@E std::cout << std::sqrt(size * p * (1 - p)) << "\n";
+//@E std::sqrt(size * p * (1 - p));
 //@E 5
 //@X
+
+//@L4 Poisson
 
 //@T dpois
 //@U std::vector&lt;double&gt; dpois(std::vector&lt;int&gt; &k, int &lambda)
@@ -1460,7 +1462,7 @@ std::vector<unsigned int> qpois(std::vector<double> &p, int &lambda) {
 //@X
 //@E unsigned int lambda = 100;
 //@E unsigned int n = 60;
-//@E std::vector<unsigned int> out = rpois(n, lambda);
+//@E std::vector&lt;unsigned int&gt; out = rpois(n, lambda);
 //@E print_nvec(out);
 //@E  :0: 114 86  86  86  115 115 85  
 //@E 85  85  116 116 84  84  119 119 
@@ -1470,11 +1472,11 @@ std::vector<unsigned int> qpois(std::vector<double> &p, int &lambda) {
 //@E 100 100 100 100 100 100 100 100
 //@E :50: 101 101 101 101 101 101 101 101 
 //@E 101 101
-//@E std::cout << Mean(out) << "\n";
+//@E Mean(out);
 //@E 99
-//@E std::cout << Sd(out) << "\n";
+//@E Sd(out);
 //@E 13.0799
-//@E std::cout << std::sqrt(lambda) << "\n";
+//@E std::sqrt(lambda);
 //@E 10
 //@X
 
@@ -3374,7 +3376,7 @@ template <typename T> unsigned int pct_to_idx(T &val, unsigned int &sizen) {
 //@D Returns the mean of all the differences in value between the contiguous elementsof a stl vector.
 //@A x : is a stl vector (int, float, double, bool)
 //@X
-//@E std::vector<double> vec = {10, 10.5, 11, 11.5};
+//@E std::vector&lt;double&gt; vec = {10, 10.5, 11, 11.5};
 //@E diff_mean(vec);
 //@E 0.5
 //@X
