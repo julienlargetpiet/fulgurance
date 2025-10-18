61d084ccfc3abfe744e5831026a43173d16eee59
Julien <pgyygp090@gmail.com>
2025-10-18 17:10:44 +0200

    added fapply_col

--- fulgurance.h
+++ fulgurance.h
@@ -7049,12 +7049,65 @@ class Dataframe{
       };
     };
 
-    //template <typename T>
-    //void fapply_int(void (&f)(T&), unsigned int &n) {
-    //  for () {
+    template <typename T>
+    void fapply_col(void (&f)(T&), unsigned int &n) {
+      unsigned int i = 2;
+      unsigned int i2;
+      bool is_found = 0;
 
-    //  };
-    //};
+      while (!is_found) {
+        i2 = 0;
+        while (i2 < matr_idx[i].size()) {
+          if (n == matr_idx[i][i2]) {
+            is_found = 1;
+            break;
+          };
+          i2 += 1;
+        };
+        i += 1;
+      };
+      
+      i -= 1;
+      i2 = nrow * i2;
+    
+      unsigned int i3 = 0;
+
+      if constexpr (std::is_same_v<T, bool>) {
+
+        for (i = i2; i < (i2 + nrow); i += 1) {
+          f(bool_v[i]);
+          tmp_val_refv[n][i] = std::to_string(bool_v[i]);
+        };
+      } else if constexpr (std::is_same_v<T, int>) {
+
+        for (i = i2; i < (i2 + nrow); i += 1) {
+          f(int_v[i]);
+          tmp_val_refv[n][i] = std::to_string(int_v[i]);
+        };
+      } else if constexpr (std::is_same_v<T, unsigned int>) {
+
+        for (i = i2; i < (i2 + nrow); i += 1) {
+          f(uint_v[i]);
+          tmp_val_refv[n][i3] = std::to_string(uint_v[i]);
+          i3 += 1;
+        };
+      } else if constexpr (std::is_same_v<T, double>) {
+        for (i = i2; i < (i2 + nrow); i += 1) {
+          f(dbl_v[i]);
+          tmp_val_refv[n][i] = std::to_string(bool_v[i]);
+        };
+      } else if constexpr (std::is_same_v<T, char>) {
+        for (i = i2; i < (i2 + nrow); i += 1) {
+          f(chr_v[i]);
+          tmp_val_refv[n][i] = std::string(1, chr_v[i]);
+        };
+      } else if constexpr (std::is_same_v<T, std::string>) {
+        for (i = i2; i < (i2 + nrow); i += 1) {
+          f(str_v[i]);
+          tmp_val_refv[n][i] = str_v[i];
+        };
+      };
+    };
 
     #if __cplusplus >= 202002L
 
