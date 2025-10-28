f72257159a60323d2eff952000e2f1fabdb3b804
Julien <pgyygp090@gmail.com>
2025-10-28 11:52:40 +0100

    added concat

--- fulgurance.h
+++ fulgurance.h
@@ -11707,6 +11707,130 @@ class Dataframe{
       dbl_v = tmp_dbl_vec;
     };
 
+    void concat(Dataframe& obj) {
+      const unsigned int& ncol2 = obj.get_ncol();
+      if (ncol != ncol2) {
+        std::cerr << "Can't concatenate 2 dataframes with different number of columns\n";
+        return;
+      };
+      const std::vector<char> type_refv2 = obj.get_typecol();
+      if (type_refv != type_refv2) {
+        std::cerr << "Can't concatenate 2 dataframes with different column type\n";
+        return;
+      };
+
+      const std::vector<std::string>& str_v2 = obj.get_str_vec();
+      const std::vector<char>& chr_v2 = obj.get_chr_vec();
+      const std::vector<bool>& bool_v2 = obj.get_bool_vec();
+      const std::vector<int>& int_v2 = obj.get_int_vec();
+      const std::vector<unsigned int>& uint_v2 = obj.get_uint_vec();
+      const std::vector<double>& dbl_v2 = obj.get_dbl_vec();
+
+      const std::vector<std::vector<std::string>>& tmp_val_refv2 = obj.get_tmp_val_refv();
+ 
+      size_t i2 = 0;
+      size_t i = 0;
+
+      const unsigned int& nrow2 = obj.get_nrow();
+      unsigned int pre_nrow = nrow;
+      nrow += nrow2;
+
+      str_v.reserve(str_v.size() + str_v2.size());
+      for (auto& el : matr_idx[0]) {
+        tmp_val_refv[el].reserve(nrow);
+        tmp_val_refv[el].insert(tmp_val_refv[el].end(), 
+                        tmp_val_refv2[el].begin(),
+                        tmp_val_refv2[el].end());
+        str_v.insert(str_v.begin() + (i2 + 1) * pre_nrow, 
+                        str_v2.begin() + i * nrow2,
+                        str_v2.begin() + (i + 1) * nrow2);
+        i2 += 2;
+        i += 1;
+      };
+
+      i2 = 0;
+      i = 0;
+      chr_v.reserve(chr_v.size() + chr_v2.size());
+
+      for (auto& el : matr_idx[1]) {
+        tmp_val_refv[el].reserve(nrow);
+        tmp_val_refv[el].insert(tmp_val_refv[el].end(), 
+                        tmp_val_refv2[el].begin(),
+                        tmp_val_refv2[el].end());
+        chr_v.insert(chr_v.begin() + (i2 + 1) * pre_nrow, 
+                        chr_v2.begin() + i * nrow2,
+                        chr_v2.begin() + (i + 1) * nrow2);
+        i2 += 2;
+        i += 1;
+      };
+
+      i2 = 0;
+      i = 0;
+      bool_v.reserve(bool_v.size() + bool_v2.size());
+
+      for (auto& el : matr_idx[2]) {
+        tmp_val_refv[el].reserve(nrow);
+        tmp_val_refv[el].insert(tmp_val_refv[el].end(), 
+                        tmp_val_refv2[el].begin(),
+                        tmp_val_refv2[el].end());
+        bool_v.insert(bool_v.begin() + (i2 + 1) * pre_nrow, 
+                        bool_v2.begin() + i * nrow2,
+                        bool_v2.begin() + (i + 1) * nrow2);
+        i2 += 2;
+        i += 1;
+      };
+
+      i2 = 0;
+      i = 0;
+      int_v.reserve(int_v.size() + int_v2.size());
+
+      for (auto& el : matr_idx[3]) {
+        tmp_val_refv[el].reserve(nrow);
+        tmp_val_refv[el].insert(tmp_val_refv[el].end(), 
+                        tmp_val_refv2[el].begin(),
+                        tmp_val_refv2[el].end());
+        int_v.insert(int_v.begin() + (i2 + 1) * pre_nrow, 
+                        int_v2.begin() + i * nrow2,
+                        int_v2.begin() + (i + 1) * nrow2);
+        i2 += 2;
+        i += 1;
+      };
+
+      i2 = 0;
+      i = 0;
+      uint_v.reserve(uint_v.size() + uint_v2.size());
+      
+      for (auto& el : matr_idx[4]) {
+        tmp_val_refv[el].reserve(nrow);
+        tmp_val_refv[el].insert(tmp_val_refv[el].end(), 
+                        tmp_val_refv2[el].begin(),
+                        tmp_val_refv2[el].end());
+        uint_v.insert(uint_v.begin() + (i2 + 1) * pre_nrow, 
+                        uint_v2.begin() + i * nrow2,
+                        uint_v2.begin() + (i + 1) * nrow2);
+        i2 += 2;
+        i += 1;
+      };
+
+      i2 = 0;
+      i = 0;
+      dbl_v.reserve(dbl_v.size() + dbl_v2.size());
+      
+      for (auto& el : matr_idx[5]) {
+        tmp_val_refv[el].reserve(nrow);
+        tmp_val_refv[el].insert(tmp_val_refv[el].end(), 
+                        tmp_val_refv2[el].begin(),
+                        tmp_val_refv2[el].end());
+        dbl_v.insert(dbl_v.begin() + (i2 + 1) * pre_nrow, 
+                        dbl_v2.begin() + i * nrow2,
+                        dbl_v2.begin() + (i + 1) * nrow2);
+        i2 += 2;
+        i += 1;
+      };
+
+      longest_determine();
+    };
+
     void set_colname(std::vector<std::string> &x) {
       if (x.size() != ncol) {
         std::cout << "the number of columns of the dataframe does not correspond to the size of the input column name vector";
@@ -13353,6 +13477,78 @@ class Dataframe{
 //@E :12: id7   1      2      3      a4
 //@E :13: id10  1      2      3      a4
 //@E :14: id14  1      2      3      NA
+//@E
+//@X
+
+//@T Dataframe.concat
+//@U void concat(Dataframe& obj)
+//@X
+//@D Concaenates the row of the dataframes. See example.
+//@A obj : is the input datarame (from which the rows will be appended)
+//@X
+//@E
+//@E  Dataframe obj1, obj2, obj3;
+//@E  std::string fname = "csv_test/outb.csv";
+//@E  obj1.readf(fname);
+//@E  obj1.display();
+//@E
+//@E     &lt;str&gt; &lt;uint&gt; &lt;uint&gt; &lt;uint&gt; &lt;str&gt;
+//@E     col1  col2   col3   col4   col5
+//@E :0:  id1   1      2      3      aa
+//@E :1:  id2   6      7      8      bb
+//@E :2:  id3   1      2      3      cc
+//@E :3:  id4   6      7      8      uu
+//@E :4:  id5   1      2      3      s4
+//@E :5:  id6   6      7      8      s9
+//@E :6:  id7   1      2      3      a4
+//@E :7:  id8   6      7      8      m9
+//@E :8:  id9   6      7      8      s9
+//@E :9:  id10  1      2      3      a4
+//@E :10: id11  6      7      8      m9
+//@E :11: id12  6      7      8      m9
+//@E :12: id13  6      7      8      s9
+//@E :13: id14  1      2      3      NA
+//@E :14: id15  16     7      8      m9
+//@E
+//@E  fname = "csv_test/outb.csv";
+//@E  obj2.readf(fname);
+//@E
+//@E  obj1.concat(obj2);
+//@E
+//@E  obj1.display();
+//@E
+//@E     &lt;str&gt; &lt;uint&gt; &lt;uint&gt; &lt;uint&gt; &lt;str&gt;
+//@E     col1  col2   col3   col4   col5
+//@E :0:  id1   1      2      3      aa
+//@E :1:  id2   6      7      8      bb
+//@E :2:  id3   1      2      3      cc
+//@E :3:  id4   6      7      8      uu
+//@E :4:  id5   1      2      3      s4
+//@E :5:  id6   6      7      8      s9
+//@E :6:  id7   1      2      3      a4
+//@E :7:  id8   6      7      8      m9
+//@E :8:  id9   6      7      8      s9
+//@E :9:  id10  1      2      3      a4
+//@E :10: id11  6      7      8      m9
+//@E :11: id12  6      7      8      m9
+//@E :12: id13  6      7      8      s9
+//@E :13: id14  1      2      3      NA
+//@E :14: id15  16     7      8      m9
+//@E :15: id1   1      2      3      aa
+//@E :16: id2   6      7      8      bb
+//@E :17: id3   1      2      3      cc
+//@E :18: id4   6      7      8      uu
+//@E :19: id5   1      2      3      s4
+//@E :20: id6   6      7      8      s9
+//@E :21: id7   1      2      3      a4
+//@E :22: id8   6      7      8      m9
+//@E :23: id9   6      7      8      s9
+//@E :24: id10  1      2      3      a4
+//@E :25: id11  6      7      8      m9
+//@E :26: id12  6      7      8      m9
+//@E :27: id13  6      7      8      s9
+//@E :28: id14  1      2      3      NA
+//@E :29: id15  16     7      8      m9
 //@E
 //@X
 
